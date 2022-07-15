eb1b0880ff06a10d69b9f1107d02fa821a1c721a
karynayen <karynayen@gmail.com>
2022-07-15 09:22:54 -0700

    trying out this new color scheme

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,35 +1,87 @@
 import './App.css';
-import { Button, Stack } from '@mui/material';
-import { IntroBox, IntroPaper, IntroHeadingTypography, IntroBodyTypography } from './styles/app.styles.js';
+import { CardMedia, GlobalStyles, Card, Stack, Box, AppBar, Toolbar, Button } from '@mui/material';
+import { IntroBox, IntroPaper, IntroHeadingTypography, IntroBodyTypography, IntroOutlinedButton } from './styles/app.styles.js';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-
+import EmailIcon from '@mui/icons-material/Email';
+//abstract later
+// make sure everything is formated for phones
+//figure out some cool animations on scrolling!!
+// background can include leaves for hiking vibes
 function App() {
   return (
     <>
+      <GlobalStyles
+        styles={{
+          body: { backgroundColor: '#C3C8EA' }
+        }}
+      />
+      {/* make a tool bar object */}
+      <Box>
+        <AppBar elevation={0} position="static">
+          <Toolbar sx={{ padding: 1, backgroundColor: '#C3C8EA' }}>
+            <Button sx={{ fontSize: 30, fontWeight: 'bold', textTransform: 'none', color: 'black' }}>Karyna Yen //</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+
       <IntroBox>
+
         <IntroPaper elevation={3}>
+
+          {/* <Card
+            sx={{
+              width: '250px',
+              height: '250px',
+              borderRadius: '50%',
+
+            }}>
+            <CardMedia
+              component="img"
+              height="250"
+              image={require('./images/me.jpg')}
+              alt="A Picture of Karyna!!"
+            />
+          </Card> */}
           {/* now edit the spacing! */}
           <IntroHeadingTypography variant="h1">
             hi, i'm <strong>karyna</strong>!
+            <br></br>
           </IntroHeadingTypography>
           <Stack spacing={2} direction="row">
             {/* how to add functionality to the buttons */}
-            <Button variant="outlined">Outlined</Button>
-            <Button variant="outlined">Outlined</Button>
+            <IntroOutlinedButton variant="outlined">Projects</IntroOutlinedButton>
+            <IntroOutlinedButton variant="outlined">Resume</IntroOutlinedButton>
+            <IntroOutlinedButton variant="outlined">Extra!</IntroOutlinedButton>
           </Stack>
 
           <IntroBodyTypography variant="body1">
-            I am a first-year computer science student at Northeastern University.
+            I am a second-year computer science student at Northeastern University,
+            interested in pursuing software developement.
+            <br></br>
+            <br></br>
+
+            I am most comfortable utilizing object-oriented design to create
+            various Java applications.
+            I also have experience using Javascript, Typescript,  HTML/CSS, and Python,
+            along with working with the React.js and Bootstrap librarys, and using Git for version control.
+            <br></br>
+            <br></br>
             In this portfolio, I will display my projects,
-            along with some of my hobbies and other projects relating to my major.
+            along with some of my hobbies and interests.
             Without further ado, welcome to my portfolio!
+            <br></br>
+            <br></br>
           </IntroBodyTypography>
           {/* make the icons clickable, CENTER THEM */}
-          <Stack spacing={2} direction="row">
+
+          <Stack spacing={2} direction="row" >
             <LinkedInIcon />
             <GitHubIcon />
+            <EmailIcon />
           </Stack>
+
 
         </IntroPaper>
       </IntroBox>
--- portfolio/src/styles/app.styles.js
+++ portfolio/src/styles/app.styles.js
@@ -1,34 +1,80 @@
 import { styled } from '@mui/material/styles'
-import { Box, Paper, Typography } from '@mui/material';
+import { Box, Paper, Typography, Button } from '@mui/material';
 
 
 // how to fit phones
 const IntroBox = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     width: '100%',
     height: '100%', // how to get screen height?!
 })
 
-const IntroPaper = styled(Paper)(({ elevation }) =>({
-    width: 400,
-    height: 1000,
-    backgroundColor: '#BBB6DF',
+const IntroPaper = styled(Paper)(({ elevation }) => ({
+    color: 'white',
+    width: '50%',
+    height: '100%',
+    backgroundColor: '#5967C5', //'#BBB6DF',
     padding: 30,
-    margin: '10% 0 0 20%',
-    
+    margin: '1% 0 0 2%',
+
 }))
 
-const IntroHeadingTypography = styled(Typography)(({ variant }) =>({
+const IntroHeadingTypography = styled(Typography)(({ variant }) => ({
     m: 1,
     fontWeight: 10,
-    fontSize: '3.5em',
+    fontSize: '4.5em',
     marginBottom: 20,
 }))
 
-const IntroBodyTypography = styled(Typography)(({ variant }) =>({
+const IntroBodyTypography = styled(Typography)(({ variant }) => ({
     marginTop: 20,
     marginBottom: 20,
     m: 1,
     fontWeight: 10,
     fontSize: '1em',
 }))
-export { IntroBox, IntroPaper, IntroHeadingTypography, IntroBodyTypography }
+
+
+const IntroContainedButton = styled(Button) ({
+    color: 'white',
+    backgroundColor: '#546e7a',
+    borderColor: 'white',
+    '&:hover': {
+        backgroundColor: '#819ca9',
+        borderColor: '#819ca9',
+        boxShadow: 'none',
+    },
+    '&:active': {
+        backgroundColor: '#819ca9',
+        borderColor: '#819ca9',
+        boxShadow: 'none',
+    },
+    '&:focus': {
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+})
+
+const IntroOutlinedButton = styled(Button)(({ variant }) => ({
+    color: 'white',
+    backgroundColor: '#5967C5', //#3f50b5',
+    borderColor: 'white',
+    fontWeight: 'bold',
+    '&:hover': {
+        backgroundColor: '#757ce8',
+        borderColor: 'white',
+        boxShadow: 'none',
+    },
+    '&:active': {
+        backgroundColor: '#757ce8',
+        borderColor: 'white',
+        boxShadow: 'none',
+    },
+    '&:focus': {
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+}))
+
+export { IntroBox, IntroPaper, IntroHeadingTypography, IntroBodyTypography, IntroContainedButton,  IntroOutlinedButton }
